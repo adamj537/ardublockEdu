c8302ca7c598ae2aa27c01bcd0958f594b801e31
Adam Johnson <adamj537@gmail.com>
2016-04-10 14:16:18 -0500

    deleted block libraries I didn't want in this distribution.

--- src/main/java/com/ardublock/translator/block/drawing/Util.java
+++ src/main/java/com/ardublock/translator/block/drawing/Util.java
@@ -2,17 +2,13 @@ package com.ardublock.translator.block.drawing;
 
 import com.ardublock.translator.Translator;
 
-
-
 public class Util
 {
 	public static void setupEnv(Translator translator)
 	{
-		
 		translator.addHeaderFile("public.h");
 		translator.addHeaderFile("common.h");
 		translator.addHeaderFile("Timer.h");
 		translator.addHeaderFile("Player.h");
-		translator.addHeaderFile("iDrawing.h");
 	}
 }
